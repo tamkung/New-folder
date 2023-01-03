79b974490330c6ae617b8ea06f890736a9950f9d
Tamkung <hm82529@gmail.com>
2023-01-03 15:17:32 +0700

    first commit

--- rentel.js
+++ rentel.js
@@ -19,54 +19,54 @@ const app = express();
 app.use(express.json());
 
 // Create a new rental
-// app.post('/rentals', (req, res) => {
-//     const { carId, startDateTime, endDateTime, rate } = req.body;
-//     const day = Math.round((new Date(endDateTime) - new Date(startDateTime)) / 8.64e7) + 1;
-//     const query = 'INSERT INTO rentals (car_id, start_date_time, end_date_time, rate, day) VALUES (?, ?, ?, ?, ?)';
-//     const values = [carId, startDateTime, endDateTime, rate, day];
-//     connection.query(query, values, (error, results) => {
-//         if (error) {
-//             if (error.code === 'ER_DUP_ENTRY') {
-//                 res.status(400).json({ error: 'Duplicate rental' });
-//             } else {
-//                 res.status(500).json({ error });
-//             }
-//         } else {
-//             res.status(201).json({ results });
-//         }
-//     });
-// });
 app.post('/rentals', (req, res) => {
     const { carId, startDateTime, endDateTime, rate } = req.body;
     const day = Math.round((new Date(endDateTime) - new Date(startDateTime)) / 8.64e7) + 1;
-    const query = 'SELECT * FROM rentals WHERE car_id = ? AND (start_date_time <= ? AND end_date_time >= ?) AND (start_date_time <= ? AND end_date_time >= ?)';
-    //const query = 'SELECT * FROM cars WHERE id NOT IN (SELECT car_id FROM rentals WHERE start_date_time <= ? AND end_date_time >= ?)';
-    const values = [carId, startDateTime, endDateTime, startDateTime, endDateTime];
+    const query = 'INSERT INTO rentals (car_id, start_date_time, end_date_time, rate, day) VALUES (?, ?, ?, ?, ?)';
+    const values = [carId, startDateTime, endDateTime, rate, day];
     connection.query(query, values, (error, results) => {
         if (error) {
-            res.status(500).json({ error });
-        } else {
-            if (results.length > 0) {
-                res.status(400).json({ error: 'Rental overlaps with existing rentals' });
+            if (error.code === 'ER_DUP_ENTRY') {
+                res.status(400).json({ error: 'Duplicate rental' });
             } else {
-                // Insert the rental into the database
-                const result = 'INSERT INTO rentals (car_id, start_date_time, end_date_time, rate, day) VALUES (?, ?, ?, ?, ?)';
-                const values2 = [carId, startDateTime, endDateTime, rate, day];
-                connection.query(result, values2, (error, results) => {
-                    if (error) {
-                        if (error.code === 'ER_DUP_ENTRY') {
-                            res.status(400).json({ error: 'Duplicate rental' });
-                        } else {
-                            res.status(500).json({ error });
-                        }
-                    } else {
-                        res.status(201).json({ results });
-                    }
-                });
+                res.status(500).json({ error });
             }
+        } else {
+            res.status(201).json({ results });
         }
     });
 });
+// app.post('/rentals', (req, res) => {
+//     const { carId, startDateTime, endDateTime, rate } = req.body;
+//     const day = Math.round((new Date(endDateTime) - new Date(startDateTime)) / 8.64e7) + 1;
+//     const query = 'SELECT * FROM rentals WHERE car_id = ? AND (start_date_time <= ? AND end_date_time >= ?) AND (start_date_time <= ? AND end_date_time >= ?)';
+//     //const query = 'SELECT * FROM cars WHERE id NOT IN (SELECT car_id FROM rentals WHERE start_date_time <= ? AND end_date_time >= ?)';
+//     const values = [carId, startDateTime, endDateTime, startDateTime, endDateTime];
+//     connection.query(query, values, (error, results) => {
+//         if (error) {
+//             res.status(500).json({ error });
+//         } else {
+//             if (results.length > 0) {
+//                 res.status(400).json({ error: 'Rental overlaps with existing rentals' });
+//             } else {
+//                 // Insert the rental into the database
+//                 const result = 'INSERT INTO rentals (car_id, start_date_time, end_date_time, rate, day) VALUES (?, ?, ?, ?, ?)';
+//                 const values2 = [carId, startDateTime, endDateTime, rate, day];
+//                 connection.query(result, values2, (error, results) => {
+//                     if (error) {
+//                         if (error.code === 'ER_DUP_ENTRY') {
+//                             res.status(400).json({ error: 'Duplicate rental' });
+//                         } else {
+//                             res.status(500).json({ error });
+//                         }
+//                     } else {
+//                         res.status(201).json({ results });
+//                     }
+//                 });
+//             }
+//         }
+//     });
+// });
 
 
 // Set up the routes for the app
