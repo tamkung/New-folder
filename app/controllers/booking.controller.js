a9b6db3cc38617956df8138c242998576e513645
Tamkung <hm82529@gmail.com>
2023-02-01 10:46:27 +0700

    booking

--- app/controllers/booking.controller.js
+++ app/controllers/booking.controller.js
@@ -2,10 +2,10 @@ const connection = require("../config/db.config");
 
 exports.addbooking = async (req, res) => {
     try {
-        const { id, uName, empoyeeNo, uPhone, uSect, uPart, note, startDateTime, endDateTime, cLicense } = req.body;
+        const { id, province, uName, empoyeeNo, uEmail, uPhone, uSect, uPart, note, startDateTime, endDateTime, bookingDate, cLicense } = req.body;
         const day = Math.round((new Date(endDateTime) - new Date(startDateTime)) / 8.64e7) + 1;
-        const query = 'INSERT INTO booking (id, uName, empoyeeNo, uPhone, uSect, uPart, note, startDateTime, endDateTime, cLicense, day) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
-        const values = [id, uName, empoyeeNo, uPhone, uSect, uPart, note, startDateTime, endDateTime, cLicense, day];
+        const query = 'INSERT INTO booking (id, province, uName, empoyeeNo, uEmail, uPhone, uSect, uPart, note, startDateTime, endDateTime, bookingDate, cLicense, day) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
+        const values = [id, province, uName, empoyeeNo, uEmail, uPhone, uSect, uPart, note, startDateTime, endDateTime, bookingDate, cLicense, day];
         connection.query(query, values, (error, results) => {
             if (error) {
                 if (error.code === 'ER_DUP_ENTRY') {
@@ -24,12 +24,30 @@ exports.addbooking = async (req, res) => {
         res.status(500).json({ message: 'Server Error!!!' });
     };
 };
+
 exports.getbooking = async (req, res) => {
     try {
         connection.query('SELECT * FROM booking', (error, results) => {
             if (error) {
                 // If an error occurred, send a server error response
                 res.status(500).json({ error });
+            } else {
+                // Otherwise, send the results as a JSON array
+                res.json(results);
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error!!!' });
+    };
+};
+
+exports.getbookingbyemail = async (req, res) => {
+    try {
+        const uEmail = req.params.id;
+        connection.query('SELECT * FROM booking WHERE uEmail = ?', [uEmail], (error, results) => {
+            if (error) {
+                // If an error occurred, send a server error response
+                res.json(results.length > 0 ? results[0] : { message: 'Booking not found' });
             } else {
                 // Otherwise, send the results as a JSON array
                 res.json(results);
--- app/routes/booking.routes.js
+++ app/routes/booking.routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 const {
     addbooking,
-    getbooking
+    getbooking,
+    getbookingbyemail
 } = require('../controllers/booking.controller');
 
 router.post("/addbooking", addbooking);
 
 router.get("/getbooking", getbooking);
+
+router.get("/getbookingbyemail/:id", getbookingbyemail);
 
 module.exports = router;
