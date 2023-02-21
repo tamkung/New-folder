1283c913c71ff37919fab973c0b4aea9f05ceb1d
Tamkung <hm82529@gmail.com>
2023-02-21 10:22:43 +0700

    port

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
     })
 })
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
